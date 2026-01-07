7c20d39b239554a7db6f79337b232482b04b9174
ToujoursPareil8 <jasonaridy@gmail.com>
2026-01-07 15:03:46 +0100

    added revised version

--- execute.c
+++ execute.c
@@ -1,26 +1,38 @@
 #include "shell.h"
 
-
-pid_t pid;
-
+/**
+ * execute_prog - Creates a child process to execute a command.
+ * @args: An array of strings representing the command and its arguments.
+ *
+ * Description: This function forks a new process. The child process
+ * attempts to execute the command, while the parent waits for its
+ * completion.
+ */
 
 void execute_prog(char **args) {
-    if (args[0] == NULL)
-    return;
-    pid =fork();
+    pid_t pid;
+    int status;
 
-    if (pid == 0) {
-        // Child process
+    if (args == NULL || args[0] == NULL)
+        return;
+
+    pid = fork();
+    if (pid == 0) 
+    {
+        /* Child process */
         if (execvp(args[0], args) == -1) {
             perror("shell");
         }
         exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        // Forking error
+    } 
+    else if (pid < 0) 
+    {
+        /* Forking error */
         perror("shell");
-    } else {
-        // Parent process
-        int status;
+    } 
+    else 
+    {
+        /* Parent process */
         waitpid(pid, &status, 0);
     }
 }
