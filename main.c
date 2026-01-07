924aa95aac0edec2e15ffced41b53fee5a91d755
Jason <jasonaridy@gmail.com>
2026-01-07 14:56:56 +0100

    added revised code

--- main.c
+++ main.c
@@ -1,19 +1,39 @@
 #include "shell.h"
 
 
-char *line = NULL;
-size_t len = 0;
-char *args[64];
 
-int main() {
+
+int main(void) 
+{
+    char *line = NULL;
+    size_t len = 0;
+    char *args[64];
+    ssize_t nread;
+
     while (1)
-    {
-        printf("$ > ");
-        if (getline(&line, &len, stdin) == -1) {
-            perror("getline");
-            break;
+    {   
+        /* Print prompt if running in interactive mode */
+        if (isatty(STDIN_FILENO))
+            printf("$ > ");
+        
+        nread = getline(&line, &len, stdin);
+
+        /* Check for end-of-file (CTRL+D) or error */
+        if (nread == -1) {
+            free(line);
+            exit(EXIT_SUCCESS);
         }
+
+        /* Remove trailing newline character */
+        if(line[nread - 1] == '\n') {
+            line[nread - 1] = '\0';
+        }
+        
         split_line(line, args);
-        execute_prog(args);
+        if (args[0] != NULL)
+            execute_prog(args);
+        
     }
+
+    return (0);
 }
