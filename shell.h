ed550c69233677302f5db5eb8ad6a8f21aa39c17
ToujoursPareil8 <jasonaridy@gmail.com>
2026-01-07 17:26:11 +0100

    added prototypes

--- shell.h
+++ shell.h
@@ -8,8 +8,11 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <sys/stat.h>
 
+extern char **environ;
 void split_line(char *line, char **args);
 void execute_prog(char **args);
+char *get_path(char *command);
 
 #endif /* SHELL_H */
