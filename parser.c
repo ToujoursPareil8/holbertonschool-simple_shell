3b828ea73a0702743ebf24fa8436dae4408006e1
ToujoursPareil8 <jasonaridy@gmail.com>
2026-01-07 15:04:14 +0100

    added revised version

--- parser.c
+++ parser.c
@@ -1,13 +1,30 @@
 #include "shell.h"
 
+
+/**
+ * split_line - Splits a string into tokens based on delimiters.
+ * @line: The string to be tokenized.
+ * @args: An array of strings where tokens will be stored.
+ *
+ * Description: This function takes a string and breaks it down into
+ * individual words (tokens) using strtok. It handles spaces, tabs,
+ * and newlines as delimiters.
+ */
 void split_line(char *line, char **args) {
     char *token;
     int position = 0;
+    /* Get the first word while ignoring whitespace */
+    token = strtok(line, " \t\r\n\a");
 
-    token = strtok(line, " \n");
-    while (token != NULL) {
+    while (token != NULL && position < 63) 
+    {
         args[position++] = token;
-        token = strtok(NULL, " \n");
+        position++;
+
+        /* Move to next token */
+        token = strtok(NULL, " \t\r\n\a");
     }
+
+    /*Last element must always be NULL for execve */
     args[position] = NULL;
 }
