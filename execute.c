220006bd1845a09f60b67e49fec4021a079affcd
Jason <jasonaridy@gmail.com>
2026-01-08 14:12:48 +0100

    fixing stuff

--- execute.c
+++ execute.c
@@ -1,49 +1,52 @@
 #include "shell.h"
 
 /**
- * execute_prog - Creates a child process to execute a command.
- * @args: An array of strings representing the command and its arguments.
+ * execute_prog - Creates a child process to execute a command
+ * @args: An array of strings representing the command and its arguments
  *
- * Description: This function forks a new process. The child process
- * attempts to execute the command, while the parent waits for its
- * completion.
+ * Return: void
  */
-
-void execute_prog(char **args) {
+void execute_prog(char **args)
+{
 	pid_t pid;
 	int status;
 	char *actual_command;
 	extern char **environ;
-	
 
 	if (args == NULL || args[0] == NULL)
 		return;
-	/* Decide if using the command or search the PATH */
-	if (args[0][0] == '/')
-		actual_command = strdup(args[0]);
-	else
-		actual_command = get_path(args[0]);
-	
-	if (actual_command == NULL) 
+
+	/* find_path uses your _getenv to locate the binary in PATH */
+	actual_command = find_path(args[0]);
+
+	if (actual_command == NULL)
 	{
-		perror("shell");
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
 		return;
 	}
 
 	pid = fork();
-	if (pid == 0) 
+	if (pid == -1)
 	{
-		/* Child process */
+		perror("fork");
+		free(actual_command);
+		return;
+	}
+
+	if (pid == 0)
+	{
+		/* Child process: execute the command */
 		if (execve(actual_command, args, environ) == -1)
 		{
-			perror("shell");
+			perror(args[0]);
+			free(actual_command);
+			exit(EXIT_FAILURE);
 		}
-		free(actual_command);
-		exit(EXIT_FAILURE);
-	} 
-	else if (pid > 0) 
+	}
+	else
 	{
+		/* Parent process: wait for child and then free memory */
 		waitpid(pid, &status, 0);
-	} 
-	free(actual_command);
+		free(actual_command);
+	}
 }
